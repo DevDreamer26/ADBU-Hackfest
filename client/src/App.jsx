d88dc61471659d3eb8d3dd929de1472f494c5149
Chinmoy Koch <chinmoykoch12@gmail.com>
2023-09-29 04:55:51 +0530

    Testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Home from './Homepage/Home'
 import Login from './register/Login'
-import SignUp from './register/signUp'
+import SignUp from ''
 import {BrowserRouter as  Route} from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
 

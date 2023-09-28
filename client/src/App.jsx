41e4eaddadfb71fc52e9f8ec09286900403ace09
Chinmoy Koch <chinmoykoch12@gmail.com>
2023-09-29 02:53:26 +0530

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import Home from './Homepage/Home'
 import Login from './register/Login'
+import SignUp from './register/signUp'
+import {BrowserRouter as  Route} from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
 
 function App() {
 
   return (
     <>
-      <Login/>
+    <Route>
+      <Home/>
+    
+      {/* <Login/>
+      <SignUp/> */}
+      </Route>
     </>
   )
 }

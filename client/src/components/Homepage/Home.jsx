80905240cd4e804bb85a24ce9d4d2bbbb45826ce
Bidyasagar <bidyasagarhazarika@gmail.com>
2023-09-29 09:54:52 +0530

    Leaderboard updated

--- client/src/components/Homepage/Home.jsx
+++ client/src/components/Homepage/Home.jsx
@@ -5,7 +5,7 @@ function Homepage() {
   return (
     <>
     <div className="container">
-      <h2 style={{color:'darkblue',fontSize:'100px'}}>Welcome to the E-Learning Platform</h2>
+      <h2 style={{color:'darkblue',fontSize:'100px'}}>Welcome to the Etutor</h2>
       <p>Find and enroll in courses to start learning.</p>
       <button >
       <a href="/courses" className="button" style={{fontSize:'40px'}}>Explore Courses</a>

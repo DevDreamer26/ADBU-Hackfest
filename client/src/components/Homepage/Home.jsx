439879f2aab925b9e2eb545241e8ab2d5c6c5424
Bidyasagar <bidyasagarhazarika@gmail.com>
2023-09-29 10:41:51 +0530

    New update

--- client/src/components/Homepage/Home.jsx
+++ client/src/components/Homepage/Home.jsx
@@ -5,15 +5,10 @@ function Homepage() {
   return (
     <div className="container">
       
-      <div className="text-container">
-        <h2>Welcome to Etutor</h2>
+      <div >
+        <h2 style={{fontSize:'96px',color:"darkblue"}}>Welcome to Etutor</h2>
         <p>Find and enroll in courses to start learning.</p>
-       
-        <div className="image-container">
-        <img src="https://cdn.pixabay.com/photo/2015/12/15/06/42/kids-1093758_1280.jpg" alt="Course Image" />
-        <a href="/courses" className="button">Explore Courses</a>
-      </div>
-      
+      <a href="/courses" className="button">Explore Courses</a>
       </div>
     </div>
   );

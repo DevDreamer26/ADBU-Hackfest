efd136edc601e38e2682f1cc22853b78923bf1fd
Chinmoy Koch <chinmoykoch12@gmail.com>
2023-09-29 05:50:42 +0530

    Quiz updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css';
-import React from 'react';
+// import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/Homepage/Home'; // Uncomment this import
-import Login from './components/register/Login'; // Uncomment this import
-import SignUp from './components/register/signUp'; // Uncomment this import
+// import Login from './components/register/Login'; // Uncomment this import
+// import SignUp from './components/register/signUp'; // Uncomment this import
 import CourseList from './components/ CourseList/ CourseList';
 import CourseDetail from './components/CourseDetail/CourseDetail';
-
+import Quiz from './components/Quiz/Quiz';
 function App() {
   return (
     <>
@@ -18,6 +18,7 @@ function App() {
           <Route path="/courses" element={<CourseList />} />
           <Route path="/coursedetails" element={<CourseDetail />} /> {/* Use a different path */}
           <Route path="/" element={<Home />} /> {/* Use a different path */}
+          <Route path="/QuizGames" element={<Quiz />} /> {/* Use a different path */}
           {/* Uncomment the following routes if needed */}
           {/* <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} /> */}

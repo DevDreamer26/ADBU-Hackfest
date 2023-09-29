71ed8afdeaecaf631fea98d4c7efb34434497ef2
Bidyasagar <bidyasagarhazarika@gmail.com>
2023-09-29 07:34:23 +0530

    Schedule

--- client/src/components/course/Course1.jsx
+++ client/src/components/course/Course1.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-
+import './Course1.css'
 function Course1() {
   return (
     <>
-    <h1>Science</h1>
-    <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius reprehenderit illum quas nam non, sunt delectus hic aut cumque deserunt perferendis suscipit temporibus accusamus quisquam. Officiis necessitatibus temporibus doloremque consequuntur error asperiores doloribus itaque eaque accusantium sint! Fuga pariatur nesciunt aperiam qui, repellendus perferendis voluptate.</div>
-    <button ><a href="/task">TASK</a>
-      
-    </button>
+   <div className="course-container">
+      <h1>Science</h1>
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius reprehenderit illum quas nam non, sunt delectus hic aut cumque deserunt perferendis suscipit temporibus accusamus quisquam. Officiis necessitatibus temporibus doloremque consequuntur error asperiores doloribus itaque eaque accusantium sint! Fuga pariatur nesciunt aperiam qui, repellendus perferendis voluptate.
+      </div>
+      <div className="button-container">
+        <a href="/task" className="course-button">Schedule</a>
+      </div>
+    </div>
     </>
   )
 }
